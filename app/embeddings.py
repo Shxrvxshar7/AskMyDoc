495d18e6c9cccc8c8b923491f788deaa511c2996
Shxrvxshar7 <arsharvesh7@gmail.com>
2026-04-30 19:48:39 +0530

    feat: switch to FastEmbed BGE embeddings for faster RAG pipeline

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,7 +1,8 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-from app.config import EMBEDDING_MODEL
+#from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import FastEmbedEmbeddings
+#from app.config import EMBEDDING_MODEL
 from functools import lru_cache
 
 @lru_cache(maxsize=1)
 def get_embedding_function():
-    return HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
+    return FastEmbedEmbeddings()
